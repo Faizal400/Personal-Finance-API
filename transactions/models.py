c5ef808c9a532fd5a25f3f7a5a271e9ac3faafff
fali009 <faizal.ali596695@gmail.com>
2026-06-25 13:49:19 +0100

    feat: transaction and category models with admin registration

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    category_name = models.TextField()
+
+class Transaction(models.Model):
+    user  = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField()
+    counterparty = models.CharField(max_length=200)
+    money_out = models.BooleanField()
+    description = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True)
+    
+
